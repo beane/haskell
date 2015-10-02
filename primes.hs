54b358600dc3785d7db24891947e857c94b781c2
Brian Deane <bdeanemachine@gmail.com>
2015-10-02 10:39:14 -0400

    finally, a prime sieve

---
+++ primes.hs
@@ -0,0 +1,34 @@
+import Data.List
+
+-- prime sieve!
+filterPrimes :: [(Integer, Integer)] -> [(Integer, Integer)]
+filterPrimes [] = []
+filterPrimes list@((n,count):xs)
+        | null list  = list
+        | count == 0 = ((head list) : (filterPrimes candidates))
+        | otherwise  = filterPrimes candidates
+        where candidates = filter stillValid $ map incCount xs
+              incCount (x,c) = if x `mod` n == 0
+                              then (x,c+1)
+                              else (x,c)
+              stillValid (x,c) = c < 1
+
+-- much sexier sieve
+-- first number in list must be 2 (the first prime)
+primeFilter :: [Integer] -> [Integer]
+primeFilter [] = []
+primeFilter list@(x:xs)
+        | null list  = list
+        | otherwise  = x : (primeFilter $ filter stillValid xs)
+        where stillValid n = if n `mod` x == 0
+                             then False
+                             else True
+
+-- want this to handle infinite lists...
+-- need to sort after
+foldPrimes :: [Integer]
+foldPrimes = foldl' step [2] [3,5..]
+        where step acc el
+                | (\x -> (el `mod` x) == 0) `any` acc = acc
+                | otherwise = el : acc
+
